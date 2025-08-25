4ae5005a0189d47e1ac7b1905e5d195539a72ece
wumingdao <wumingdao070@gmail.com>
2025-08-25 16:03:43 +0800

    01 challenged 1

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -142,11 +142,11 @@ const heightMark = 1.88;
 const massJohn = 85;
 const heightJohn = 1.76;
 
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-const markHigherBMI = BMIMark > BMIJohn;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn, markHigherBMI);
+// console.log(BMIMark, BMIJohn, markHigherBMI);
 
 ////////////////////////////////////
 // Strings and Template Literals
---
+++ 01-Fundamentals-Part-1/starter/challenge_1.js
@@ -0,0 +1,49 @@
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+*/
+
+// Test data 1
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+
+// Test data 2
+// const markWeight = 95;
+// const markHeight = 1.88;
+// const johnWeight = 85;
+// const johnHeight = 1.76;
+
+// const markBMI = markWeight / (markHeight * markHeight);
+// const johnBMI = johnWeight / (johnHeight * johnHeight);
+// const markHigherBMI = markBMI > johnBMI;
+
+// console.log(`Mark's BMI is ${markBMI}, John's BMI is ${johnBMI}`);
+
+// console.log(`Mark has a higher BMI than John: ${markHigherBMI}`);
+
+// course version
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark, BMIJohn, markHigherBMI);
